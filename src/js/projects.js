f2be2a5e8808c3d2e627c2d9c7664a9116a7cff3
OlenaKononenko23 <148655722+OlenaKononenko23@users.noreply.github.com>
2024-04-17 18:48:44 +0300

    section

---
+++ src/js/projects.js
@@ -0,0 +1,57 @@
+import Swiper from 'swiper/bundle';
+
+import 'swiper/css';
+
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    navigation: {
+    nextEl: '.projects-button-slider-next.swiper-button-next',
+    prevEl: '.projects-button-slider-prev.swiper-button-prev',
+},
+    keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+},
+    on: {
+    reachEnd: function () {
+        document.querySelector('.projects-button-slider-next.swiper-button-next svg use')
+        .setAttribute('href', '/img/symbol-defs.svg#icon-arrow-narrow-right');
+
+        var nextButton = document.querySelector('.projects-button-slider-next');
+        nextButton.style.borderColor = 'grey';
+    },
+    reachBeginning: function () {
+        document.querySelector('.projects-button-slider-prev.swiper-button-prev svg use')
+        .setAttribute('href', '/img/symbol-defs.svg#icon-arrow-narrow-right');
+
+        var prevButton = document.querySelector('.projects-button-slider-prev');
+        prevButton.style.borderColor = 'grey';
+    },
+    slideChange: function(){
+        if (!swiper.isBeginning && !swiper.isEnd) {
+
+        document.querySelector('.projects-button-slider-next.swiper-button-next svg use')
+        .setAttribute('href', '/img/symbol-defs.svg#icon-arrow-right-white');
+        document.querySelector('.projects-button-slider-prev.swiper-button-prev svg use')
+        .setAttribute('href', '/img/symbol-defs.svg#icon-arrow-right-white');
+
+
+        var prevButton = document.querySelector('.projects-button-slider-prev');
+        var nextButton = document.querySelector('.projects-button-slider-next');
+        prevButton.style.borderColor = 'white';
+        nextButton.style.borderColor = 'white';
+        }
+    }
+    },
+});
+
+document.addEventListener('keydown', function(event) {
+      if (event.keyCode === 9) {
+            event.preventDefault();
+            if (event.shiftKey) {
+          swiper.slidePrev();
+        } else {
+          swiper.slideNext();
+        }
+      }
+    }); 
