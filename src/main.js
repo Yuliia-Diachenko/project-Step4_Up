93f85ea41c22792088410c71f30e5b483ac0f3d4
Yuliia Diachenko <zirkanew82@gmail.com>
2024-04-17 11:16:14 +0300

    add-lib

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-// import Accordion from 'accordion-js';
-// import 'accordion-js/dist/accordion.min.css';
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
 
-// import Swiper from 'swiper';
-// import 'swiper/css';
-// import axios from 'axios';
+import Swiper from 'swiper';
+import 'swiper/css';
+import axios from 'axios';
 
-// import iziToast from "izitoast";
-// import "izitoast/dist/css/iziToast.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
 import "./js/header.js";
 import "./js/api.js";
